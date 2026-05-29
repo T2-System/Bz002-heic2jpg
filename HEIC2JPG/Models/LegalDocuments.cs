62f34622164755980f8c7d9e955705316e76166e
t_ogura <t_ogura@t2system.co.jp>
2026-05-29 17:56:09 +0900

    feat: 利用規約対応

---
+++ HEIC2JPG/Models/LegalDocuments.cs
@@ -0,0 +1,13 @@
+namespace HEIC2JPG.Models;
+
+public static class LegalDocuments
+{
+    public const string TermsVersion = "2026-06-01";
+    public const string ConsentVersion = "2026-06-01";
+
+    public const string ConsentVersionStorageKey = "heic2jpg.legalConsentVersion";
+    public const string ConsentAcceptedAtStorageKey = "heic2jpg.legalConsentAcceptedAt";
+
+    public const string ContactUrl = "https://www.t2system.co.jp/";
+    public const string PrivacyPolicyUrl = "https://www.t2system.co.jp/privacy_policy/";
+}
